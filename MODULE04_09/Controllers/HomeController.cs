ee19e7cb474f9f4b761f42778bc0f7253d2da2ef
van chung chen <auxo_86@hotmail.com>
2017-05-23 03:00:14 +0800

    加入專案檔案。

---
+++ MODULE04_09/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MODULE04_09.DAL;
+
+namespace MODULE04_09.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Get: Home/ProductByID/1
+        //Get: Home/ProductByID?ID=1
+        public ActionResult ProductByID(int id)
+        {
+            NorthwindEntities db = new NorthwindEntities();
+            var query = from p in db.Products
+                        where p.ProductID == id
+                        select p;
+
+            //只列第一筆
+            var result = query.First();
+
+            return View(result);
+        }
+
+        public ActionResult ProductsByCategory(int id)
+        {
+            NorthwindEntities db = new NorthwindEntities();
+            var query = from p in db.Products
+                        where p.CategoryID == id
+                        select p;
+
+            
+            var result = query.ToList();
+
+            return View(result);
+        }
+    }
+}
